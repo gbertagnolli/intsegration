29b45b991390d2a17dbdf512829a4490deef7d88
Giulia Bertagnolli <giulia.bertagnolli@gmail.com>
2021-05-17 11:44:07 +0200

    new version

--- R/GCE.R
+++ R/GCE.R
@@ -1,18 +1,18 @@
 ####################################################
-# Global Communication Efficeiency: function for evaluation of GCE
+# Global Communication Efficiency: function for evaluation of GCE
 #
-# Version: 0.4
-# Last update: Jan 2020
+# Version: 1.0
+# Last update: May 2021
 # Authors: Giulia Bertagnolli
 #
 # History:
 #
-# Dependences:
+# Dependencies:
 #
-# igraph, e1071
+# igraph, Rcpp
 #
 ####################################################
-#' @title Global Communication Efficeiency
+#' @title Global Communication Efficiency
 #' @description The average inverse shortest path length is a measure known as
 #'    the global efficiency (see Latora and Marchiori, 2001).
 #'    We implement the global communication efficiency (GCE) for weighted
@@ -42,12 +42,31 @@
 #'    if weights is NULL (the default) and g has a weight edge
 #'    attribute that will be passed to the igraph
 #'    \code{\link[igraph]{distances}} function, as \eqn{w_{ij}^{-1}}.
+#'    If this is NA then no weights are used (even if the graph has a weight attribute).
+#'    The normalisation of the GCE relises on the comparison with the efficiency of a
+#'    \eqn{G_{ideal}}, characterised by the weighted adjacency matrix
+#'    \eqn{G_{ideal} = \frac{W + \Phi}{2}}. See [2] for more details.
+#' @param lengths Euclidean length of the edge, i.e., Euclidean distance between its
+#'    two end-points, which are assumed to be non-negative reals. If this is NULL (the default)
+#'    and \code{g} has an edge attribute \code{length} that will be used.
+#'    If this is NA then no lengths are used (even if the graph edges have a length attribute).
+#'    Mind that the GCE normalised over Euclidean lengths/distances has
+#'    \eqn{W_{ideal} = \Phi_{Eucl}}.
 #' @param verbose logical, default TRUE.
 #' @return A list
 #'   \itemize{
 #'     \item non_normalised - communication efficiency \eqn{E(G)},
 #'     \item normalised - normalised communication efficiency, \eqn{GCE(G)},
-#'       or NULL if normalised is \code{FALSE}.
+#'       or NULL if \code{normalised} is \code{FALSE}. If the network is topological (i.e. no flows)
+#'       or if \code{weights = NA} then the normalised topological efficiency is equal to the
+#'       non-normalised topological efficiency.
+#'     \item normalised_lengths - normalised communication efficiency w.r.t. edge lengths,
+#'       i.e. \eqn{GCE^{Eucl}(G)}: shortest-paths are computed using the flows (as in the usual
+#'       normalied version), but now the path total cost and total strength are, respectively,
+#'       the sum of Euclidean distances (lengths) and the sum of the reciprocals of the lengths of
+#'       edges over the paths. NULL if there are non lengths.
+#'       If \code{normalised = FALSE} but \code{lengths} are provided, then the \eqn{GCE^{Eucl}(G)}
+#'       (that is \code{normalised_lengths}) will be computed anyway.
 #'   }
 #' @keywords communication efficiency; integration;
 #' @references \describe{
@@ -65,14 +84,16 @@
 #' GCE(karate, directed = F)
 #' @export
 GCE <- function(g, directed = FALSE, normalised = TRUE, weights = NULL,
-  verbose = TRUE) {
+                lengths = NULL, verbose = TRUE) {
   # create return object
   res <- list(
     "non_normalised" = NULL,
-    "normalised" = NULL
+    "normalised" = NULL,
+    "normalised_lengths" = NULL
   )
-  # aux variable
-  topological <- F
+  # aux variables
+  topological <- FALSE
+  use_lengths <- TRUE
   # directed or undirected graph
   if ((igraph::is.directed(g)) && (!directed)) g <- igraph::as.undirected(g)
   # set weight and inverse-of-weight attributes
@@ -109,60 +130,136 @@ GCE <- function(g, directed = FALSE, normalised = TRUE, weights = NULL,
     g <- igraph::set_edge_attr(g, name = "weight", value = 1)
     g <- igraph::set_edge_attr(g, name = "weight_inv", value = 1)
   }
+  # Euclidean distances
+  if (is.null(lengths)) {
+    # default case
+    if ("length" %in% igraph::edge_attr_names(g)) {
+      # using length edge attribute
+      if (verbose) {
+        cat("Using length edge attribute.")
+      }
+      # igraph::E(g)$length # length is an edge cost
+      # 1 / igraph::E(g)$length # reciprocal of lengths are edge strengths
+      g <- igraph::set_edge_attr(g, name = "length_inv",
+                                 value = (1. / igraph::E(g)$length))
+    } else {
+      # no (numeric) length edge attribute
+      if (verbose) {
+        cat("No Euclidean lengths/distances provided.")
+      }
+      use_lengths <- FALSE
+      # g <- igraph::set_edge_attr(g, name = "length", value = 1)
+      # g <- igraph::set_edge_attr(g, name = "length_inv", value = 1)
+    }
+  } else if (length(lengths) == 1 && is.na(lengths)) {
+    if (verbose) cat("Ignoring edge lengths.")
+    use_lengths <- FALSE
+    # g <- igraph::set_edge_attr(g, name = "length", value = 1)
+    # g <- igraph::set_edge_attr(g, name = "length_inv", value = 1)
+  } else if (is.numeric(lengths)) {
+    # providing new edge lengths
+    if (verbose) {
+      cat("Adding given lengths as edge costs and 1 / lengths as edge strengths.\n")
+    }
+    igraph::E(g)$length <- lengths
+    igraph::E(g)$length_inv <- 1. / igraph::E(g)$length
+  } else {
+    warning("Issues with edge lengths. They are currently ignored.")
+    use_lengths <- FALSE
+    # g <- igraph::set_edge_attr(g, name = "length", value = 1)
+    # g <- igraph::set_edge_attr(g, name = "length_inv", value = 1)
+  }
   # Check for multiedges and loops
   if (!igraph::is.simple(g)) {
-    cat("graph is not simple. Aggrgating (sum) multiedges, removing
-        self-loops.\n")
-    g <- igraph::simplify(g, edge.attr.comb = list("weight" = "sum"))
+    cat("graph is not simple. Aggrgating multiedges (sum of weights, min of lengths),
+        removing self-loops.\n")
+    min_na_rm <- function(x) {
+      return(min(x, na.rm = TRUE))
+    }
+    g <- igraph::simplify(g, edge.attr.comb = list(
+                               "weight" = "sum",
+                               "length" = "min_na_rm")
+                               )
     igraph::E(g)$weight_inv <- 1. / igraph::E(g)$weight
+    igraph::E(g)$length_inv <- 1. / igraph::E(g)$length
   }
   # ---
   # Auxiliary objects
   # ---
   N <- igraph::gorder(g)
   A <- igraph::as_adjacency_matrix(g, attr = "weight")
-  # ---
-  # Create empty matrix Phi (if normalised = T)
-  # ---
-  if (normalised) {
-    Phi <- matrix(NA, nrow = N, ncol = N)
+  if (topological) {
+    normalised <- FALSE
   }
   # ---
   # Start computing shortest-paths
   if (verbose) cat("Start computation of shortest-paths\n")
   x <- as.matrix(igraph::as_adjacency_matrix(g, attr = "weight_inv"))
-  x[x == 0] <- Inf
-  g_all_shortest_paths <- e1071::allShortestPaths(x)
-  rm(x)
-  D <- g_all_shortest_paths$length
-  diag(D) <- NA
-  # ---
-  if (verbose) cat("Computing matrix Phi\n")
-  if (normalised) {
-    if (topological) {
-      Phi <- D
-      Phi[is.na(Phi)] <- 0
-      diag(Phi) <- NA
-    } else {
-      for (i in 1:(N - 1)) {
-        if (!directed) {
-          j0 <- i + 1
-        } else {
-          j0 <- 1
-        }
-        for (j in c(j0:N)) {
-          Phi[i, j] <- sum(extract_paths_weights(g_all_shortest_paths, A, i, j))
-        }
-      }
-      if (!directed) {
-        Phi[lower.tri(Phi)] <- t(Phi)[lower.tri(t(Phi))]
-      }
-    }
+  x[x == 0] <- .Machine$double.xmax         # almost as <- Inf
+  if (!use_lengths) {
+    # case without Euclidean lengths/distances
+    g_all_shortest_paths <- rcpp_floyd_flow(x)
+    rm(x)
+  } else {
+    # case with Euclidean lengths/distances
+    l <- as.matrix(igraph::as_adjacency_matrix(g, attr = "length"))
+    l[l == 0] <- .Machine$double.xmax         # almost as <- Inf
+    g_all_shortest_paths <- rcpp_floyd_flow_length(x, l)
+    rm(x)
+    rm(l)
   }
+  # split outputs
+  # shortest-path distances
+  D <- g_all_shortest_paths$D
+  D[D == .Machine$double.xmax] <- NA
+  diag(D) <- NA
+  if (normalised) {
+    # total flows along shortest-paths
+    Phi <- g_all_shortest_paths$F
+    diag(Phi) <- NA
+  }
+  if (use_lengths) {
+    # total (sum of) Euclidean distance along (the same) shortest-paths
+    D_eucl <- g_all_shortest_paths$D_eucl
+    D_eucl[D_eucl == .Machine$double.xmax] <- NA
+    diag(D_eucl) <- NA
+    # sum of reciprocal Euclidean distances along (the same) shortest-paths
+    Phi_eucl <- g_all_shortest_paths$F_eucl
+    diag(Phi_eucl) <- NA
+  }
+  rm(g_all_shortest_paths)
+  # ---
+  # if (verbose) cat("Computing matrix Phi\n")
+  # if (normalised) {
+  #   if (topological) {
+  #     Phi <- D
+  #     Phi[is.na(Phi)] <- 0
+  #     diag(Phi) <- NA
+  #   } else {
+  #     for (i in 1:(N - 1)) {
+  #       if (!directed) {
+  #         j0 <- i + 1
+  #       } else {
+  #         j0 <- 1
+  #       }
+  #       for (j in c(j0:N)) {
+  #         Phi[i, j] <- sum(extract_paths_weights(g_all_shortest_paths, A, i, j))
+  #       }
+  #     }
+  #     if (!directed) {
+  #       Phi[lower.tri(Phi)] <- t(Phi)[lower.tri(t(Phi))]
+  #     }
+  #   }
+  # }
   if (verbose) cat("compute E(G)\n")
   E <- 1. / N / (N - 1) * sum(1. / D, na.rm = T)
   # save E(G) in res[["non_normalised"]]
   res$non_normalised <- E
+  if (topological) {
+    # if there are no flows, the topological efficiency is already normalised, hence
+    # non_normalised and normalised efficencies are equal in the topological case.
+    res$normalised <- E
+  }
   if (normalised) {
     # also compute the normalised GCE
     W_ideal <- .5 * (A + Phi)
@@ -176,6 +273,19 @@ GCE <- function(g, directed = FALSE, normalised = TRUE, weights = NULL,
             and pairs.\n")
       }
       res$normalised <- NA
+    }
+  }
+  if (use_lengths) {
+    E_eucl <- 1. / N / (N - 1) * sum(1. / D_eucl, na.rm = T)
+    E_eucl_ideal <- 1. / N / (N - 1) * sum(Phi_eucl, na.rm = T)
+    if (!identical(E_eucl_ideal, E_eucl)) {
+      res$normalised_lengths <- E_eucl / E_eucl_ideal
+    } else {
+      if (verbose) {
+        cat("G is not a true network, but a collaction of isolated nodes
+            and pairs.\n")
+      }
+      res$normalised_lengths <- NA
     }
   }
   if (!igraph::is_connected(g)) {
@@ -192,12 +302,12 @@ GCE <- function(g, directed = FALSE, normalised = TRUE, weights = NULL,
   return(res)
 }
 
-# auxiliary function
-extract_paths_weights <- function(obj, adj, start, end) {
-  sp <- e1071::extractPath(obj, start, end)
-  res <- numeric(0)
-  for (i in 2:length(sp)) {
-    res <- c(res, adj[sp[i - 1], sp[i]])
-  }
-  return(res)
-}
+# # auxiliary function
+# extract_paths_weights <- function(obj, adj, start, end) {
+#   sp <- e1071::extractPath(obj, start, end)
+#   res <- numeric(0)
+#   for (i in 2:length(sp)) {
+#     res <- c(res, adj[sp[i - 1], sp[i]])
+#   }
+#   return(res)
+# }
---
+++ src/rcpp_floyd_flow_length.cpp
@@ -0,0 +1,59 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// Implementation of Floyd-Warshall algorithm for shortest-path distances (sum of costs)
+// Additionally, compute also the total flow (sum of strengths) along those paths and
+// the Euclidean length of shortest-paths (sum of Euclidean lengths) and
+// the total Euclidean closeness (sum of reciprocals Euclidean lengths) along sh.-paths.
+
+//' Floyd-Warshall algorithm for shortest-path distances and total flows
+//'
+//' Additionally, it computes also the Euclidean length of shortest-paths (sum of Euclidean lengths)
+//' and the total Euclidean closeness (sum of reciprocals Euclidean lengths) along shortest-paths.
+//' @param C numeric matrix of edge costs
+//' @param L numeric matrix of edge (Euclidean) lenghts
+//' @return D matrix of shortest-path distances, i.e. sum of costs along minimising paths
+//' @return F matrix of total flows, i.e. sum of inverse costs, along shortest-paths
+//' @return D_eucl matrix of Euclidean distances along shortest-paths (paths minimising sum of costs)
+//' @return F_eucl matrix of total Eucl. flows, i.e. sum of inverse lengths, along shortest-paths
+// [[Rcpp::export]]
+List rcpp_floyd_flow_length(NumericMatrix C, NumericMatrix L) {
+  List res;
+  int n = C.nrow();
+  NumericMatrix D(n, n);
+  NumericMatrix F(n, n);
+  NumericMatrix D_eucl(n, n);
+  NumericMatrix F_eucl(n, n);
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      D[i + n * j] = C[i + n * j];
+      F[i + n * j] = 1 / C[i + n * j];
+      D_eucl[i + n * j] = L[i + n * j];
+      F_eucl[i + n * j] = 1 / L[i + n * j];
+    }
+  }
+  for (int i = 0; i < n; i++) {
+    D[i + n * i] = 0;              /* no self cycle */
+    F[i + n * i] = 0;              /* no self cycle */
+    D_eucl[i + n * i] = 0;         /* no self cycle */
+    F_eucl[i + n * i] = 0;         /* no self cycle */
+  }
+  for (int k = 0; k <n; k++) {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (D[i + n * k] + D[k + n * j] < D[i + n * j]) {
+          D[i + n * j] = D[i + n * k] + D[k + n * j];
+          F[i + n * j] = F[i + n * k] + F[k + n * j];
+          D_eucl[i + n * j] = D_eucl[i + n * k] + D_eucl[k + n * j];
+          F_eucl[i + n * j] = F_eucl[i + n * k] + F_eucl[k + n * j];
+        }
+      }
+    }
+  }
+  res["D"] = D;
+  res["F"] = F;
+  res["D_eucl"] = D_eucl;
+  res["F_eucl"] = F_eucl;
+  return(res);
+}
